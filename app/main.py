4e61aa90b28bc2c2c0dd2258dcae7c3b7817e8d5
Jackpot <brunoruizn@gmail.com>
2025-12-19 13:14:57 -0300

    feat: app lifespan along tables creation
    - initial version 0.1.0
    - swagger ui parameters for token persistency

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.core.db import engine
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+    yield
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan, 
+    title="BEGamer components", 
+    version="0.1.0",
+    swagger_ui_parameters={"persistAuthorization": True})
 
 
 @app.get("/health")
