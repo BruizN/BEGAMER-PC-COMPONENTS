b58f7f4fea4c7d1cf898e0b1c3b853172dab9487
Jackpot <brunoruizn@gmail.com>
2025-12-27 11:19:18 -0300

    feat(auth): add auth router with login endpoint
    - add include router to app

---
+++ app/modules/auth/router.py
@@ -0,0 +1,23 @@
+from fastapi import Depends, APIRouter
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Annotated
+from app.modules.auth.schemas import TokenOut, LoginRequest
+from app.core.dependencies import SessionDep #Dependencia de sesion asincrona
+from app.modules.auth.service import login_user
+
+router = APIRouter()
+
+
+@router.post(
+    "/login", 
+    response_model=TokenOut, 
+    summary="Inicia sesion con email y contraseña"
+    )
+async def login(session: SessionDep, body: LoginRequest):
+    token = await login_user(session, body.email, body.password)
+    return {"access_token": token, "token_type": "bearer"}
+
+
+
+
+
