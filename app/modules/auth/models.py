4ad55c06e57e486ac47e407aa4c2349ed93a3c5e
Jackpot <brunoruizn@gmail.com>
2025-12-27 11:14:07 -0300

    fix(auth): change user_id type to uuid to solve pydantic validation error

--- app/modules/auth/models.py
+++ app/modules/auth/models.py
@@ -1,6 +1,7 @@
 from app.modules.auth.schemas import UserBase
 from enum import Enum 
-from sqlmodel import SQLModel, Field, AutoString
+from sqlmodel import Field, AutoString
+import uuid
 import uuid6
 
 class Role(str, Enum):
@@ -8,7 +9,7 @@ class Role(str, Enum):
     CLIENT = "cliente"
 
 class User(UserBase, table=True):
-    id: uuid6.UUID = Field(default_factory=uuid6.uuid7, primary_key=True)
+    user_id: uuid.UUID = Field(default_factory=uuid6.uuid7, primary_key=True)
     role: Role = Field(sa_type=AutoString) #Guarda el enum como String para evitar problemas de migraciones con PostgreSQL
     hashed_password: str
     is_active: bool = True
