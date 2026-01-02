997b6a9ba2b179c52a058c041b0229833f705484
Jackpot <brunoruizn@gmail.com>
2026-01-02 13:20:42 -0300

    feat(schemas): implement category create, read and update models
    - Define `CategoryCreate` to handle input validation.
    - Define `CategoryRead` to include generated IDs in responses.
    - Define `CategoryUpdate` for updates.
    This separation ensures better security and API documentation.

--- app/modules/catalog/schemas.py
+++ app/modules/catalog/schemas.py
@@ -4,3 +4,11 @@ from sqlmodel import SQLModel, Field
 class CategoryBase(SQLModel):
     name: str = Field(unique=True, index=True)
 
+class CategoryCreate(CategoryBase):
+    pass
+
+class CategoryRead(CategoryBase):
+    category_id: uuid.UUID
+
+class CategoryUpdate(CategoryBase):
+    pass
