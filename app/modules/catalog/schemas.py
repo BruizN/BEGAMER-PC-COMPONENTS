e73d1478a6afc43ec52df6d41fba620ab5e950be
Jackpot <brunoruizn@gmail.com>
2026-01-15 09:48:21 -0300

    feat(catalog): add code field and extra validations for code and name

--- app/modules/catalog/schemas.py
+++ app/modules/catalog/schemas.py
@@ -1,9 +1,41 @@
 from sqlmodel import SQLModel, Field
+from pydantic import field_validator
 import uuid
+import re
 
 
-class CategoryBase(SQLModel):
-    name: str = Field(unique=True, index=True)
+class HasCodeMixin(SQLModel):
+    code: str = Field(unique=True, index=True)
+
+    @field_validator("code")
+    @classmethod
+    def validate_code(cls, v: str | None) -> str | None:
+        # Si es None (en un PATCH), retornasin validar
+        if v is None:
+            return v
+            
+        v = v.upper().strip()
+        if not (2 <= len(v) <= 4):
+            raise ValueError("The code must be between 2 and 4 characters long.")
+        if not re.match("^[A-Z0-9]+$", v):
+            raise ValueError("The code can only contain letters and numbers.")
+        return v
+
+class HasNameMixin(SQLModel):
+    name: str = Field(unique=True, index=True, min_length=3, max_length=30)
+
+    @field_validator("name")
+    @classmethod
+    def validate_name(cls, v: str | None) -> str | None:
+        if v is None:
+            return v
+            
+        v = v.strip()
+        return v.title()
+
+
+class CategoryBase(HasNameMixin, HasCodeMixin, SQLModel):
+    pass
 
 class CategoryCreate(CategoryBase):
     pass
@@ -11,11 +43,13 @@ class CategoryCreate(CategoryBase):
 class CategoryRead(CategoryBase):
     category_id: uuid.UUID
 
-class CategoryUpdate(CategoryBase):
-    pass
+class CategoryUpdate(HasNameMixin, HasCodeMixin, SQLModel):
+    # Sobrescribe los campos para hacerlos opcionales.
+    name: str | None = Field(default=None, min_length=3, max_length=30)
+    code: str | None = Field(default=None)
 
-class BrandBase(SQLModel):
-    name: str = Field(unique=True, index=True)
+class BrandBase(HasNameMixin, HasCodeMixin, SQLModel):
+    pass
 
 class BrandCreate(BrandBase):
     pass
@@ -23,5 +57,7 @@ class BrandCreate(BrandBase):
 class BrandRead(BrandBase):
     brand_id: uuid.UUID
 
-class BrandUpdate(BrandBase):
-    pass
+class BrandUpdate(SQLModel):
+    name: str | None = Field(default=None, min_length=3, max_length=30)
+    code: str | None = Field(default=None)
+
