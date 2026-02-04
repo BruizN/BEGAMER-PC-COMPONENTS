138bd3d8c0153724432b1604e2999e3ddc196114
bruno <brunoruizn@gmail.com>
2026-02-04 10:10:33 -0300

    feat(models): Add migration with models that incorporate auditmixin inheritance

--- app/modules/catalog/models.py
+++ app/modules/catalog/models.py
@@ -1,11 +1,12 @@
 from sqlmodel import Field
+from app.core.AuditMixin import AuditMixin
 from app.modules.catalog.schemas import CategoryBase, BrandBase
 import uuid
 import uuid6
 
 
-class Category(CategoryBase, table=True):
+class Category(CategoryBase, AuditMixin, table=True):
     category_id: uuid.UUID = Field(default_factory=uuid6.uuid7, primary_key=True)
 
-class Brand(BrandBase, table=True):
+class Brand(BrandBase, AuditMixin, table=True):
     brand_id: uuid.UUID = Field(default_factory=uuid6.uuid7, primary_key=True)
