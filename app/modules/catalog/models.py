4eb8c76f21028675b19acbda2d9cebaa4e5198cf
Bruno Ruiz <brunoruizn@gmail.com>
2026-02-12 11:32:45 -0300

    Refactor/validation strategy (#39)
    * refactor(schemas): replace HasName/Code Mixins with new annotated types
    
    now schema cleans data and models define sql restrictions.
    
    * tests: Update tests to adapt to new data sanitization
    
    remove .title() for name field in conftest, and change some data tests texts to match the new data sanitization

--- app/modules/catalog/models.py
+++ app/modules/catalog/models.py
@@ -6,15 +6,20 @@ import uuid6
 
 
 class Category(CategoryBase, AuditMixin, table=True):
+    name: str = Field(unique=True, index=True)
+    code: str = Field(unique=True, index=True)
     category_id: uuid.UUID = Field(default_factory=uuid6.uuid7, primary_key=True)
     products: list["Product"] = Relationship(back_populates="category", passive_deletes=True)
 
 class Brand(BrandBase, AuditMixin, table=True):
+    name: str = Field(unique=True, index=True)
+    code: str = Field(unique=True, index=True)
     brand_id: uuid.UUID = Field(default_factory=uuid6.uuid7, primary_key=True)
     products: list["Product"] = Relationship(back_populates="brand", passive_deletes=True) #evita la actualizacion de hijos a none al eliminar el padre
 
 class Product(ProductBase, AuditMixin, table=True):
     product_id: uuid.UUID = Field(default_factory=uuid6.uuid7, primary_key=True)
+    name: str = Field(unique=True, index=True)
     slug: str = Field(unique=True, index=True)
     category_id: uuid.UUID = Field(foreign_key="category.category_id")
     brand_id: uuid.UUID = Field(foreign_key="brand.brand_id")
